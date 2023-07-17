37267cbaed6097eb7b9425faaf4bed028f65735a
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-17 08:15:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+let employee = {name: 'Joshua Mwaniki', streetAddress: 'Githunguri'};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+      };
+    }
+    
+updateEmployeeWithKeyAndValue(employee, 'name', 'Mary Mwaniki')
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'name', 'Michael Mukoshi')
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newestEmployee = {...employee};
+    delete newestEmployee[key];
+    return newestEmployee;
+}
+deleteFromEmployeeByKey(employee, 'streetAddress')
+console.log(newestEmployee);
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+delete employee[key];
+return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee, 'name')
+console.log(employee)
